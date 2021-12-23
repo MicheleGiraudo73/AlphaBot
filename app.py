00e9dea2f233aeb949c15bd1100fded434e93348
AlbyAa03 <61047024+AlbyAa03@users.noreply.github.com>
2021-12-23 09:22:28 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,177 @@
+from flask import Flask, render_template, request
+import threading as thr
+import time
+app = Flask(__name__)
+
+'''
+import RPi.GPIO as GPIO
+class AlphaBot(object):
+    
+    def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
+        self.IN1 = in1
+        self.IN2 = in2
+        self.IN3 = in3
+        self.IN4 = in4
+        self.ENA = ena
+        self.ENB = enb
+        self.PA  = 50
+        self.PB  = 50
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA,500)
+        self.PWMB = GPIO.PWM(self.ENB,500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()
+
+    def right(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def stop(self):
+        self.PWMA.ChangeDutyCycle(0)
+        self.PWMB.ChangeDutyCycle(0)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def left(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+
+    def backward(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def forward(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        
+    def set_pwm_a(self, value):
+        self.PA = value
+        self.PWMA.ChangeDutyCycle(self.PA)
+
+    def set_pwm_b(self, value):
+        self.PB = value
+        self.PWMB.ChangeDutyCycle(self.PB)    
+        
+    def set_motor(self, left, right):
+        if (right >= 0) and (right <= 100):
+            GPIO.output(self.IN1, GPIO.HIGH)
+            GPIO.output(self.IN2, GPIO.LOW)
+            self.PWMA.ChangeDutyCycle(right)
+        elif (right < 0) and (right >= -100):
+            GPIO.output(self.IN1, GPIO.LOW)
+            GPIO.output(self.IN2, GPIO.HIGH)
+            self.PWMA.ChangeDutyCycle(0 - right)
+        if (left >= 0) and (left <= 100):
+            GPIO.output(self.IN3, GPIO.HIGH)
+            GPIO.output(self.IN4, GPIO.LOW)
+            self.PWMB.ChangeDutyCycle(left)
+        elif (left < 0) and (left >= -100):
+            GPIO.output(self.IN3, GPIO.LOW)
+            GPIO.output(self.IN4, GPIO.HIGH)
+            self.PWMB.ChangeDutyCycle(0 - left)
+'''
+'''
+    def gestisci(self,parola):
+        #self.funzioni = {"F":self.forward,"S":self.stop,"B":self.backward,"L":self.left,"R":self.right}
+        #try:
+         #   self.funzioni[parola]()
+        #except:
+        #    pass
+        
+        conn = None
+        try:
+            conn = sqlite3.connect(path)
+        except:    
+            print("file non trovato")
+            
+        cur = conn.cursor()
+        comandi = None
+        try:
+            cur.execute(f"SELECT sequenza FROM movimenti WHERE nome = '{parola}'")
+            comandi = cur.fetchall()[0][0]
+            print(comandi)
+        except:
+            print("query non valida")
+        
+        self.funzioni = {"F":self.forward,"S":self.stop,"B":self.backward,"L":self.left,"R":self.right}
+        for comando in comandi.split(";"):
+            try:
+                self.funzioni[comando[0]]()
+            except:
+                pass
+            time.sleep(float(comando[1:])/1000)
+            self.stop()
+'''
+
+class AlphaBot(object):
+    def __init__(self):
+        pass
+    def forward(self):
+        print("forward")
+    def backward(self):
+        print("backword")       
+    def right(self):
+        print("right")
+    def left(self):
+        print("left")
+    def stop(self):
+        print("stop")
+    def gestisci(self,comando):
+        print(comando)
+        print("dio")
+    
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    bot = AlphaBot()
+    
+    if request.method == 'POST':
+        
+        if(request.form['inputQuery'] != ''):
+            #print(request.form['inputQuery'])
+            bot.gestisci(request.form['inputQuery'])
+        elif request.form.get('avanti') == '↑':
+            bot.forward()
+        elif  request.form.get('indietro') == '↓':
+            bot.backward()
+        elif  request.form.get('destra') == '→':
+            bot.right()
+        elif  request.form.get('sinistra') == '←':
+            bot.left()
+        elif  request.form.get('stop') == 'stop':
+            bot.stop()
+        else:
+            print("ciao")
+    elif request.method == 'GET':
+        return render_template('index.html')
+    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost')
